0a356dcff350fcc7be3e7a6ff5ada5f8d3d97e14
aapolkovsky <aapolkovsky@mail.ru>
2018-06-25 16:22:30 +0300

    Add helpers and checks for body type

--- bodyparser.go
+++ bodyparser.go
@@ -1,8 +1,6 @@
 package bodyparser
 
 import (
-	"context"
-	"encoding/json"
 	"net/http"
 	"reflect"
 )
@@ -21,30 +19,14 @@ type BodyParser struct {
 
 // New creates a new bodyparser middleware for the provided type.
 func New(bodyType reflect.Type) *BodyParser {
+	if !isValidType(bodyType) {
+		return nil
+	}
+
 	return &BodyParser{
 		bodyType:       bodyType,
 		proceedOnError: false,
 	}
-}
-
-// Get returns parsed body or parsing error
-func Get(ctx context.Context) (interface{}, error) {
-	switch v := ctx.Value(BodyKey).(type) {
-	case error:
-		return nil, v
-	default:
-		return v, nil
-	}
-}
-
-// GetBody returns parsed body
-func GetBody(ctx context.Context) interface{} {
-	return ctx.Value(BodyKey)
-}
-
-// GetError returns parsing error
-func GetError(ctx context.Context) error {
-	return ctx.Value(BodyKey).(error)
 }
 
 // ProceedOnError configures bodyparser middleware to proceed on parsing error
@@ -62,34 +44,39 @@ func (bodyParser *BodyParser) OnError(onError func(http.ResponseWriter, *http.Re
 // Handler apply the bodyparser middleware on the request
 func (bodyParser *BodyParser) Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Create instance of type 'bodyType' and return pointer to that as interface
-		body := reflect.New(bodyParser.bodyType).Interface()
-
-		// Decode request body to 'body' variable
-		err := json.NewDecoder(r.Body).Decode(body)
-		defer r.Body.Close()
+		body, err := Parse(r.Body, bodyParser.bodyType)
 
 		if err != nil {
 			if bodyParser.proceedOnError {
 				// Populate context with error
-				r = r.WithContext(context.WithValue(r.Context(), BodyKey, err))
+				r = r.WithContext(Put(r.Context(), err))
+
+				// Serve next
 				next.ServeHTTP(w, r)
 				return
 			}
 
 			if bodyParser.onError != nil {
 				// Populate context with error
-				r = r.WithContext(context.WithValue(r.Context(), BodyKey, err))
+				r = r.WithContext(Put(r.Context(), err))
+
+				// Call special error handler
 				bodyParser.onError(w, r)
 				return
 			}
 
+			// Respond with "Bad Request" error
 			http.Error(w, "Bad Request", http.StatusBadRequest)
 			return
 		}
 
+		// Close request body
+		defer r.Body.Close()
+
 		// Populate context with body
-		r = r.WithContext(context.WithValue(r.Context(), BodyKey, body))
+		r = r.WithContext(Put(r.Context(), body))
+
+		// Serve next
 		next.ServeHTTP(w, r)
 	})
 }
---
+++ helpers.go
@@ -0,0 +1,81 @@
+package bodyparser
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"reflect"
+)
+
+var invalidTypes = []reflect.Kind{
+	reflect.Invalid,
+	reflect.Complex64,
+	reflect.Complex128,
+	reflect.Chan,
+	reflect.Func,
+	reflect.Ptr,
+	reflect.UnsafePointer,
+}
+
+// ErrInvalidType indicates that bodyType is invalid
+var ErrInvalidType = errors.New("bodyparser: Invalid type of body")
+
+func isValidType(bodyType reflect.Type) bool {
+	kind := bodyType.Kind()
+
+	for _, t := range invalidTypes {
+		if kind == t {
+			return false
+		}
+	}
+	return true
+}
+
+// Get returns parsed body or parsing error
+func Get(ctx context.Context) (interface{}, error) {
+	switch v := ctx.Value(BodyKey).(type) {
+	case error:
+		return nil, v
+	default:
+		return v, nil
+	}
+}
+
+// GetBody returns parsed body
+func GetBody(ctx context.Context) interface{} {
+	return ctx.Value(BodyKey)
+}
+
+// GetError returns parsing error
+func GetError(ctx context.Context) error {
+	return ctx.Value(BodyKey).(error)
+}
+
+// Put puts data (body or error) to context
+func Put(ctx context.Context, data interface{}) context.Context {
+	return context.WithValue(ctx, BodyKey, data)
+}
+
+// NewOfType creates new instance of bodyType and returns it as interface
+func NewOfType(bodyType reflect.Type) (interface{}, error) {
+	if !isValidType(bodyType) {
+		return nil, ErrInvalidType
+	}
+
+	// Put pointer to type of body for other types
+	return reflect.New(bodyType).Interface(), nil
+}
+
+// Parse parses bytes from reader to new instance of bodyType
+func Parse(reader io.Reader, bodyType reflect.Type) (interface{}, error) {
+	body, err := NewOfType(bodyType)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(reader).Decode(body)
+
+	return body, err
+}
